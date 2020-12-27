56ed7e9ed92b84315ceb635c877138de24eab283
sherriconner50 <76406387+sherriconner50@users.noreply.github.com>
2020-12-27 16:03:13 -0500

    updated verbiage

--- main/views.py
+++ main/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(response):
-    return HttpResponse("<H1>This is Test 2</H1>")
+    return HttpResponse("<H1>You have reached page 1</H1>")
 
 def v1(response):
-    return HttpResponse("<H1>View 1 test</H1>")
+    return HttpResponse("<H1>You are looking at an unauthorized view!</H1>")
